4c89722676b6e3c661bc4fd7203d8f69251068a4
Viktor Sopilniak <sopilniak@gmail.com>
2018-01-11 01:22:40 +0200

    first beta)

--- js/scrolling-to-fixed-header.js
+++ js/scrolling-to-fixed-header.js
@@ -1,17 +1,20 @@
 $(function () {
-    var shrinkHeader = 30;
+    // var shrinkHeader = 30;
     $(window).scroll(function () {
-        var scroll = getCurrentScroll();
-        if (scroll >= shrinkHeader) {
+        let scroll = getCurrentScroll();
+        // console.log(scroll);
+        if (scroll > 0) {
             $('.logo-wrapper').addClass('shrink-box');
             $('.logo-wrapper__slogan').addClass('shrink-slogan');
             $('.logo-wrapper__header').addClass('shrink-header');
             $('.logo-wrapper__tel').addClass('shrink-tel');
+            $('.scrollHint').addClass('div-no-display');
         } else {
             $('.logo-wrapper').removeClass('shrink-box');
             $('.logo-wrapper__slogan').removeClass('shrink-slogan');
             $('.logo-wrapper__header').removeClass('shrink-header');
             $('.logo-wrapper__tel').removeClass('shrink-tel');
+            $('.scrollHint').removeClass('div-no-display');
         }
     });
 
